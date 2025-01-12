0b1616cd3e256fcec5158d926bae9dbcdf056033
thetechrobo <thetechrobo@atlantic>
2025-01-12 22:06:58 +0000

    Changes that have been on my server for awhile now

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import *
+from quart import *
 
-import time
+import asyncio, random
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 @app.route("/")
-def hello():
-    def generate():
+async def hello():
+    async def generate():
         yield """
         <style>
             div {
@@ -18,7 +18,7 @@ def hello():
                 height: 480px;
             }
         </style><div>
-        <img class="hello" src="https://dl.fireon.live/irc/5ebfd1b86e2db220/:3.png" /></div>
+        <img class="h" src="https://lounge.thetechrobo.ca/uploads/9d883830f824eadf/image.png" /></div>
         """
         X = 0
         Y = 0
@@ -26,13 +26,18 @@ def hello():
         stepY = 2
         viewportH = 480 - 223
         viewportW = 640 - 213
+        adjustment = 0
         while True:
             if X >= viewportW or X < 0:
+                adjustment = random.randint(0, 360)
+                yield '<style>.h { filter: hue-rotate(%sdeg) }</style>\n' % adjustment
                 stepX *= -1
             if Y >= viewportH or Y < 0:
+                adjustment = random.randint(0, 360)
+                yield '<style>.h { filter: hue-rotate(%sdeg) }</style>\n' % adjustment
                 stepY *= -1
             X += stepX
             Y += stepY
-            yield '<style>.hello { transform: translate(%s, %s) }</style>\n' % (f"{X}px", f"{Y}px")
-            time.sleep(0.01)
+            yield '<style>.h { transform: translate(%s,%s) }</style>\n' % (f"{X}px", f"{Y}px")
+            await asyncio.sleep(0.01)
     return app.response_class(generate(), mimetype='text/html')
